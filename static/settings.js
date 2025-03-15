98fe84b75628f0315c890c9fb247faa903667cae
Eken2008 <oskar.eklov@gmail.com>
2025-03-15 18:40:06 +0100

    Refactor settings management by creating a reusable function for bang groups, improve settings display transitions and fix #1

--- static/settings.js
+++ static/settings.js
@@ -1,58 +1,75 @@
+const createBangGroup = (bang) => {
+    // Create a group for bang
+    const group = document.createElement("div");
+    group.classList.add("settingsBangGroup");
+
+    // Input for bang name
+    const inputBang = document.createElement("input");
+    inputBang.classList.add("settingsBang");
+    inputBang.disabled=true;
+    inputBang.value = bang.t;
+    group.appendChild(inputBang);
+
+    // Input for bang URL
+    const inputUrl = document.createElement("input");
+    inputUrl.classList.add("settingsUrl");
+    inputUrl.value = bang.u.replace("{{{s}}}", "%s").replace("https://","");
+    inputUrl.disabled=true;
+    group.appendChild(inputUrl);
+
+    // Button to remove bang
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML="-";
+    removeButton.classList.add("settingsBangButton");
+    removeButton.onclick = () => {
+        // Get the URL and bang name to remove
+        const url = removeButton.parentElement.querySelector(".settingsUrl").value;
+        const bang = removeButton.parentElement.firstElementChild.value;
+
+        const localBangs = JSON.parse(localStorage.getItem("localBangs"));
+        for(const storedBang of localBangs){
+            if (storedBang.u.replace("https://","")===url.replace("%s","{{{s}}}")&&storedBang.t===bang){
+                // Remove bang that matches the URL and name and reopen settings
+                localBangs.splice(localBangs.indexOf(storedBang),1);
+                localStorage.setItem("localBangs", JSON.stringify(localBangs));
+                loadBangs();
+                removeButton.parentElement.remove();
+                break;
+            }
+        }
+    }
+    group.appendChild(removeButton);
+    
+    return group;
+}
+
 const openSettings = () => {
     // Reset input fields
     document.querySelector(".customBangs").innerHTML="";
     document.querySelector(".defaultBangInput").value=getDefaultBang();
     for (const bang of JSON.parse(localStorage.getItem("localBangs"))) {
-
-        // Create a group for bang
-        const group = document.createElement("div");
-        group.classList.add("settingsBangGroup");
-
-        // Input for bang name
-        const inputBang = document.createElement("input");
-        inputBang.classList.add("settingsBang");
-        inputBang.disabled=true;
-        inputBang.value = bang.t;
-        group.appendChild(inputBang);
-
-        // Input for bang URL
-        const inputUrl = document.createElement("input");
-        inputUrl.classList.add("settingsUrl");
-        inputUrl.value = bang.u.replace("{{{s}}}", "%s").replace("https://","");
-        inputUrl.disabled=true;
-        group.appendChild(inputUrl);
-
-        // Button to remove bang
-        const removeButton = document.createElement("button");
-        removeButton.innerHTML="-";
-        removeButton.classList.add("settingsBangButton");
-        removeButton.onclick = () => {
-            // Get the URL and bang name to remove
-            const url = removeButton.parentElement.querySelector(".settingsUrl").value;
-            const bang = removeButton.parentElement.firstElementChild.value;
-
-            const localBangs = JSON.parse(localStorage.getItem("localBangs"));
-            for(const storedBang of localBangs){
-                if (storedBang.u.replace("https://","")===url.replace("%s","{{{s}}}")&&storedBang.t===bang){
-                    // Remove bang that matches the URL and name and reopen settings
-                    localBangs.splice(localBangs.indexOf(storedBang),1);
-                    localStorage.setItem("localBangs", JSON.stringify(localBangs));
-                    loadBangs();
-                    // TODO: Modify dom instead of reopening settings
-                    openSettings();
-                    break;
-                }
-            }
-        }
-        group.appendChild(removeButton);
+        const group = createBangGroup(bang);
 
         document.querySelector(".customBangs").appendChild(group);
     }
 
     // Show settings
+    document.querySelector(".settingsBg").style.opacity="0";
     document.querySelector(".settingsBg").style.display="flex";
+    requestAnimationFrame(()=>
+        requestAnimationFrame(()=>
+            document.querySelector(".settingsBg").style.opacity="1"
+        )
+    );
 }
 
+const closeSettings = () => {
+    // Hide settings
+    document.querySelector(".settingsBg").style.opacity="0";
+    setTimeout(() => {
+        document.querySelector(".settingsBg").style.display="none";
+    }, 100);
+}
 
 
 window.addEventListener("load", () => {
@@ -61,11 +78,11 @@ window.addEventListener("load", () => {
     document.querySelector(".settingsBg").onclick = (e) => {
         // Close settings if clicked outside
         if (!document.querySelector(".settings").matches(":hover")){
-            document.querySelector(".settingsBg").style.display="none";
+            closeSettings();
         }
     }
     document.querySelector(".closeButton").onclick = () => {
-        document.querySelector(".settingsBg").style.display="none";
+        closeSettings();
     }
 
 
@@ -103,8 +120,16 @@ window.addEventListener("load", () => {
         e.target.parentElement.firstElementChild.value="";
         e.target.parentElement.querySelector(".settingsUrl").value="";
         
-        // TODO: Modify dom instead of reopening settings
-        openSettings();
+        const group = createBangGroup({t:bang,u:url});
+        document.querySelector(".customBangs").prepend(group);
+
         loadBangs();
     }        
+});
+
+
+window.addEventListener("keydown", (e) => {
+    if (e.key==="Escape"){
+        closeSettings();
+    }
 });
